9f423bf110c1e6ee242d07eb215fbbf3540193df
Muskan Sharma <70201126+MuskanSharma08@users.noreply.github.com>
2020-10-13 14:42:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var ball;
+var db, pos;
+
+function setup(){
+    createCanvas(500,500);
+    db = firebase.database();
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    db.ref("ball/position").on("value", readPos);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+  db.ref("ball/position").set({
+      x: pos.x + x,
+      y: pos.y + y, 
+  })    
+}
+function readPos(data){
+    pos = data.val();
+ball.x = pos.x;
+ball.y = pos.y;    
+}
